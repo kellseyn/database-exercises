5762ba93ab70eb0af5d7e0cd45a45389386b9eae
Kellsey Neagley <kellseyneagley@gmail.com>
2019-12-17 15:34:07 -0600

    completed tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_db;
+CREATE TABLE IF NOT EXISTS albums(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR (50) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  release_date DATE,
+  sales FLOAT NOT NULL,
+  genre VARCHAR (50) NOT NULL,
+  PRIMARY KEY(id)
+);
